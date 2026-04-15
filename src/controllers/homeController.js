61372a6f167ac350b8c7f0bba4ab61a0701643d9
beginerdesginer901 <dattrinh901@gmail.com>
2026-04-15 16:30:57 +0700

    Connection pool

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,24 @@
+const connection = require('../config/database')
+
 const getHomePage = (req, res) => {
     //process data
     //call model
-    res.send('Hello World!')
+    //res.send('Hello World!')
+
+    let user = [];
+    connection.query(
+        'Select * from Users u',
+        (err, results, fields) => {
+            if (err) {
+                console.log(">>> err:", err);
+                return;
+            }
+            user = results;
+            console.log(">>> results:", results); // results contains rows returned by server
+
+           res.send(JSON.stringify(user));
+        }
+    );
 }
 
 const getABC = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -4,16 +4,27 @@ require('dotenv').config()
 
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const connection = require('./config/database')
 
 const app = express () // Khởi tạo một Express app bằng cách gọi hàm express(). Biến app sẽ đại diện cho ứng dụng của bạn và được sử dụng để định nghĩa các route và cấu hình khác cho server.
 const port = process.env.PORT || 8081 // port
 const hostname = process.env.HOST_NAME || 'localhost' // hostname
+
 
 //config template engine
 configViewEngine(app)
 
 //khai báo route 
 app.use('/test', webRoutes) 
+
+// const connection = mySql.createConncetion({
+//     host: process.env.DB_HOST ,
+//     port: process.env.DB_PORT,         //default 3306
+//     user: process.env.DB_USER,       //default empty
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// })
+
 
 app.listen(port, hostname, () => {
     console. log( `Example app listening on port ${port}` )
